17037f7cae19ad49cb367c0d1346b2ad4b2daa42
Eduardo P de Sousa <edupsousa@gmail.com>
2018-03-26 08:44:02 -0300

    Write repository name and relative file path

--- src/ArchitectureVisitor.java
+++ src/ArchitectureVisitor.java
@@ -31,15 +31,26 @@ public class ArchitectureVisitor implements CommitVisitor {
 	@Override
 	public void process(SCMRepository repository, Commit commit, PersistenceMechanism writer) {
 		List<RepositoryFile> files = repository.getScm().files();
+		String repositoryName = getRepositoryName(repository.getOrigin());
+		String repositoryPath = repository.getPath();
 		for (RepositoryFile file : files) {
+			String filePath = relativePath(repositoryPath, file.getFullName());
 			Map<String, String> roleMap = this.mappingStrategy.applyHeuristics(file);
 			if (roleMap.size() == 0) {
-				writer.write(file.getFullName(), "unknown", "");
+				writer.write(repositoryName, filePath, "unknown", "");
 			} else {
 				for (Map.Entry<String, String> entry : roleMap.entrySet()) {
-					writer.write(file.getFullName(), entry.getKey(), entry.getValue());
+					writer.write(repositoryName, filePath, entry.getKey(), entry.getValue());
 				}
 			}
 		}
+	}
+	
+	protected String getRepositoryName(String origin) {
+		return origin.substring("https://github.com/".length(), origin.length()-4);
+	}
+	
+	protected String relativePath(String repositoryPath, String filePath) {
+		return filePath.substring(repositoryPath.length()+1);
 	}
 }
