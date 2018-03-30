b5733e862e9354ecf4d8ddd4f503101046df5a61
Eduardo P de Sousa <edupsousa@gmail.com>
2018-03-30 12:33:48 -0300

    Added repo directories

--- src/ArchitectureStudy.java
+++ src/ArchitectureStudy.java
@@ -16,9 +16,12 @@ public class ArchitectureStudy implements Study {
 	@Override
 	public void execute() {
 		String[] columns = {"project", "file", "role", "heuristic"};
-		PersistenceMechanism outputFile = new SQLFile("~/Documents/Mestrado/jsf.sql", "roles", columns, false);
+		PersistenceMechanism outputFile = new SQLFile("~/Documents/Mestrado/roles.sql", "roles", columns, false);
 		new RepositoryMining()
-			.in(GitRepository.allProjectsIn("/home/edupsousa/Documents/Mestrado/Projetos/jsf/"))
+			//.in(GitRepository.allProjectsIn("/home/edupsousa/Documents/Mestrado/Projetos/spring_rest/"))
+			//.in(GitRepository.allProjectsIn("/home/edupsousa/Documents/Mestrado/Projetos/jsf/"))
+			//.in(GitRepository.allProjectsIn("/home/edupsousa/Documents/Mestrado/Projetos/spring/"))
+			.in(GitRepository.singleProject("/home/edupsousa/Documents/Mestrado/Projetos/spring/239444"))
 			.through(Commits.onlyInHead())
 			.process(ArchitectureVisitor.createAndConfigure("./resources/heuristics.json") , outputFile)
 			.mine();
